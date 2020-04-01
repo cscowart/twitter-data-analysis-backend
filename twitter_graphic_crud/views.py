9cbc6ca31acb848db728473c30118fe1d48c56ba
cscowart <chasecowart@gmail.com>
2020-04-01 15:11:40 -0500

    adding queryset

--- twitter_graphic_crud/views.py
+++ twitter_graphic_crud/views.py
@@ -7,17 +7,6 @@ class GraphicList(generics.ListCreateAPIView):
     queryset = Graphic.objects.all().order_by('date_created')
     serializer_class = GraphicSerializer
 
-    def get_queryset(self):
-        """
-        This view should return a list of all the purchases
-        for the currently authenticated user.
-        """
-        queryset = Graphic.objects.all()
-        title = self.request.query_params.get('search', None)
-        if title is not None:
-            queryset = queryset.filter(title__ilike=title)
-        return queryset
-
 class GraphicDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Graphic.objects.all().order_by('date_created')
     serializer_class = GraphicSerializer
