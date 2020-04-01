0571e513c8c4f38540894b2706e02e340f93ca43
cscowart <chasecowart@gmail.com>
2020-04-01 15:16:14 -0500

    adding search method 2

--- twitter_graphic_crud/views.py
+++ twitter_graphic_crud/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import filters
 
 from .serializers import GraphicSerializer
 from .models import Graphic
@@ -6,6 +7,8 @@ from .models import Graphic
 class GraphicList(generics.ListCreateAPIView):
     queryset = Graphic.objects.all().order_by('date_created')
     serializer_class = GraphicSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author']
 
 class GraphicDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Graphic.objects.all().order_by('date_created')
